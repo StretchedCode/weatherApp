b1f7162dbdaaf9651c97f6b367764e7cc022a7aa
Ghufran <ghufranshahid101@gmail.com>
2023-06-13 16:20:12 -0400

    Added imports and eventlistener for form button. connected functions to one another

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('hello');
+import { initialLoad, setValues } from './functions/dom';
+import processData from './functions/process';
+import getWeather from './functions/weather';
+// inital loading of DOM
+
+initialLoad();
+
+const formButton = document.querySelector('form > button');
+const searchInput = document.querySelector('#searchInput');
+
+formButton.addEventListener('click', async (e) => {
+  e.preventDefault();
+  const data = await getWeather(searchInput.value);
+  const weatherObject = processData(data);
+  searchInput.value = '';
+  setValues(weatherObject);
+});
