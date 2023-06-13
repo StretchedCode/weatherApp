745a145477200c9b83734031d1f89dc2e4175874
Ghufran <ghufranshahid101@gmail.com>
2023-06-13 17:02:12 -0400

    Added https

--- src/functions/weather.js
+++ src/functions/weather.js
@@ -1,7 +1,7 @@
 // module that facilitates receiving all of the data from the WeatherAPI
 
 async function getWeather(query) {
-  const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=593c4e658973493595534108231206&q=${query}`, { mode: 'cors' });
+  const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=593c4e658973493595534108231206&q=${query}`, { mode: 'cors' });
   const weatherData = await response.json();
 
   return weatherData;
